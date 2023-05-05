5495a5786a2f37118cd7b1dc00b759c8446267cc
Ryan Apodaca <rapodaca1@my.hpu.edu>
2023-05-04 16:20:45 -1000

    first commit

---
+++ src/main/java/com/recipes/demo/models/SiteUser.java
@@ -0,0 +1,48 @@
+package com.recipes.demo.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+    String userName;
+    String password;
+    @OneToMany(mappedBy = "siteUser")
+    List<Recipe> recipes;
+
+    protected SiteUser (){}
+
+    public siteUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "siteUser{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
